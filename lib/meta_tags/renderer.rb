3dcade0d3d9f1f35498e4480a6fe4aeac91eaae8
David <david@humanoide.co>
2019-06-28 20:26:40 -0300

    change renderer order

--- lib/meta_tags/renderer.rb
+++ lib/meta_tags/renderer.rb
@@ -22,17 +22,17 @@ module MetaTags
 
       render_charset(tags)
       render_title(tags)
-      render_icon(tags)
       render_with_normalization(tags, :description)
       render_with_normalization(tags, :keywords)
       render_refresh(tags)
       render_noindex(tags)
       render_alternate(tags)
       render_open_search(tags)
-      render_links(tags)
 
       render_hashes(tags)
       render_custom(tags)
+      render_icon(tags)
+      render_links(tags)
 
       tags.tap(&:compact!).map! { |tag| tag.render(view) }
       view.safe_join tags, MetaTags.config.minify_output ? "" : "\n"
